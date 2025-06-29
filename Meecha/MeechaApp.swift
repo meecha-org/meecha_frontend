b4af47fdab4523b72a565b3485f6786463c3cfe8
りんご <hanacoi7243@gmail.com>
2025-06-29 21:55:19 +0900

    Initial Commit

---
+++ Meecha/MeechaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeechaApp.swift
+//  Meecha
+//
+//  Created by 2230220 on 2025/06/29.
+//
+
+import SwiftUI
+
+@main
+struct MeechaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
