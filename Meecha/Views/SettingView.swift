8553958d1e8e7beb5ac12b3eaa96e982a74516f7
mattuu0 <tempacountyt@gmail.com>
2025-07-18 01:03:25 +0900

    ログアウト機能を追加

--- Meecha/Views/SettingView.swift
+++ Meecha/Views/SettingView.swift
@@ -9,11 +9,15 @@ import SwiftUI
 
 struct SettingView: View {
     @State var isDistance: Bool = false
+    
+    @State var UserName: String = ""
+    @State var UserID: String = ""
+    
     var body: some View {
         if isDistance{ MapWrapperView() }
         else{
             VStack(spacing: 32) {
-                SettingAccountInfo(Myicon: .myicon, MyName: "りんご", MyID: "1234567890")
+                SettingAccountInfo(Myicon: .myicon, MyName: UserName, MyID: UserID)
                     .padding(.top, 150)
                 
                 RoundedRectangle(cornerRadius: 5)
@@ -34,6 +38,19 @@ struct SettingView: View {
                     SettingListsGroup(isDistance: $isDistance)
                 }
                 Spacer()
+            }.task {
+                do {
+                    // 自身の情報取得
+                    let response = try await FetchInfo()
+                    
+                    debugPrint("userInfo: \(response)")
+                    
+                    // 情報を設定
+                    UserName = response.name
+                    UserID = response.userId
+                } catch {
+                    debugPrint(error)
+                }
             }   // VStack
         }
     }   // body
